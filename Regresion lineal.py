530942a569f35af7c4e644c06d6f1739e141e3db
luis marquez <luisandres248@gmail.com>
2020-07-17 00:08:04 -0300

    PyE rev5

--- Regresion lineal.py
+++ Regresion lineal.py
@@ -9,8 +9,10 @@ import matplotlib.pyplot as plt
 import seaborn as seabornInstance
 from sklearn.metrics import mean_squared_error
 
+#archivo de datos
 datafile = 'datosRL.xls'
 
+#sacando datos del archivo hacia un dataframe
 data = pd.read_excel(datafile, header=1)
 print(data)
 data2=data.transpose()
@@ -18,37 +20,40 @@ data2.dropna(axis=0,inplace=True)
 data2.rename(columns=data2.iloc[0],inplace=True)
 data2.drop(['Año'],inplace=True)
 print(data2)
-
-
-
 print(data2.describe())
 
-data2.plot(x='consumo', y='Renta Neta', style='o')
-plt.title('Renta Neta vs Consumo')
-plt.xlabel('Consumo')
-plt.ylabel('Renta Neta')
+#ploteo de los datos en diagrama de puntos
+data2.plot(x='Renta Neta', y='consumo', style='o')
+plt.title('Consumo vs Renta')
+plt.xlabel('Renta Neta')
+plt.ylabel('Consumo')
 
-plt.show()
-
+#datos del dataframe a 2 listas
 consumo_list = data2['consumo'].to_list()
 renta_list= data2['Renta Neta'].to_list()
 
-x = np.array(consumo_list).reshape((-1,1))
-y = renta_list
-
+#modelo de regresion lineal
+x = np.array(renta_list).reshape((-1,1))
+y = consumo_list
 model = LinearRegression()
 model.fit(x,y)
 
+#coeficiente de determinacion y coeficientes de recta
 r_sq = model.score(x,y)
 print('coefficient of determination:', r_sq)
-
 print('beta = ' + str(model.coef_) + ', alpha = ' + str(model.intercept_))
+
 
 # Predecimos los valores y para los datos usados en el entrenamiento
 prediccion_entrenamiento = model.predict(x)
+print(prediccion_entrenamiento)
+print(type(prediccion_entrenamiento))
+y2 = np.array(y)
+print(y2)
+print(type(y2))
 
 # Calculamos el Error Cuadrático Medio (MSE = Mean Squared Error)
-mse = mean_squared_error(y_true = y, y_pred = prediccion_entrenamiento)
+mse = mean_squared_error(y_true = y2, y_pred = prediccion_entrenamiento)
 
 # La raíz cuadrada del MSE es el RMSE
 rmse = np.sqrt(mse)
